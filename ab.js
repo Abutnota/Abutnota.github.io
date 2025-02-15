b20a76d4bfcb7c1d3457ad6c35003fe07c0d8b7e
HDK <duckhanh24062008@gmail.com>
2025-02-15 21:51:07 +0700

    Add files via upload

---
+++ ab.js
@@ -0,0 +1,11 @@
+ var p=0;
+function myFunction() {
+ if(p==0) {
+ document.body.style.backgroundImage ="url('BGr/sunset.gif')";
+  p=1;
+ }
+  else {
+ document.body.style.backgroundImage ="url('BGr/abc.gif')" ;
+ p=0;
+ }
+}
