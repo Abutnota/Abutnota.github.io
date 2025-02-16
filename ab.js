d426385a66b785053816c90f159592938a6c4768
HDK <duckhanh24062008@gmail.com>
2025-02-16 12:38:12 +0700

    Update ab.js

--- ab.js
+++ ab.js
@@ -1,17 +1,17 @@
  var p=0;
 function myFunction() {
  switch(p){
- default:
- document.body.style.backgroundImage ="url('BGr/sunset.gif')";
-  p=1;
-  break;
   case 1:
  document.body.style.backgroundImage ="url('BGr/fireplace.gif')" ;
  p=2;
  break;
   case 2:
-  document.body.style.backgroundImage ="url('BGr/abc.gif')" ;
+ document.body.style.backgroundImage ="url('BGr/abc.gif')" ;
   p=0;
+  break;
+   default:
+ document.body.style.backgroundImage ="url('BGr/sunset.gif')";
+  p=1;
   break;
  }
 }
